17036a29f4a202f3eb2a48acee58e73366a8f930
JaisAndersen <jais.h.sorensen@gmail.com>
2024-12-08 10:26:01 +0100

    Connection string

--- Models/BUMSDbContext.cs
+++ Models/BUMSDbContext.cs
@@ -6,7 +6,7 @@ namespace BUMS.Models
     {
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BUMSULogin;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            options.UseSqlServer(@"Data Source=mssql11.unoeuro.com;Initial Catalog=jaand_dk_db_bums;User ID=jaand_dk;Password=kbm3ydDrAF9cap4n2E6H;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
         }
         public DbSet<User> Users { get; set; }
         public DbSet<Group> Groups { get; set; }
