a440a00de77b5153eaa0754d318f443eb5b4ff57
Maskere <Mstachkruse@gmail.com>
2024-12-10 14:27:32 +0100

    Commit

--- Models/UserGroup.cs
+++ Models/UserGroup.cs
@@ -10,8 +10,5 @@ namespace BUMS
         public User User { get; set; }
         public int GroupID { get; set; }
         public Group Group { get; set; }
-
-        //public int AccessID { get; set; }
-        //public Access Access { get; set; }
     }
 }
